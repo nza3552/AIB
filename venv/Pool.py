ef277c5a4550c28a281f6859fdbddbcdca67820c
nza3552 <nza3552@rit.edu>
2019-04-10 19:25:38 -0400

    todo: apply friction tl all balls make more than one ball write the state of the game

--- venv/Pool.py
+++ venv/Pool.py
@@ -13,7 +13,6 @@ import pymunk.pygame_util
 from Table import Table
 from Ball import Ball
 class Pool:
-
     BALL_RADIUS = 23
 
     FOOT_SPOT = (600, 200)
@@ -51,61 +50,85 @@ class Pool:
         (16, (255, 255, 255), False)]
 
     def main(self):
-        table = Table()
+        self.sunkBalls = []
+        self.points = 0
+        self.table = Table()
+        self.ready = True
 
         ball1 = Ball(16, (255, 0, 0, 255), True)
-        ball1.place(table.space, (200,200))
+        ball1.place(self.table.space, (200,200))
 
-        # ball2 = Ball(2, (255, 255, 0), True)
-        # ball2.place(table.space, (300, 220))
-        collHand = table.space.add_collision_handler(10, 11)
+        collHand = self.table.space.add_collision_handler(10, 11)
         collHand.begin = self.callback
         while(True):
-            # print(collHand.separate)
-            # while(not collHand.separate == None):
-            #     print(collHand.separate)
-            table.display()
+            self.table.display()
+
             events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     exit()
-            if table.textinput.update(events):
-                if table.textStatus == "Angle":
-                    table.angle = table.textinput.input_string
-                    table.textinput.clear_text()
-                    table.textStatus = "Power"
-                    table.angleEntered = True
+            if self.table.textinput.update(events):
+                if self.table.textStatus == "Angle":
+                    self.table.angle = self.table.textinput.input_string
+                    self.table.textinput.clear_text()
+                    self.table.textStatus = "Power"
+                    self.table.angleEntered = True
                 else:
-                    table.textStatus = "Angle"
-                    table.power = table.textinput.input_string
-                    table.textinput.clear_text()
-                    table.powerEntered = True
+                    self.table.textStatus = "Angle"
+                    self.table.power = self.table.textinput.input_string
+                    self.table.textinput.clear_text()
+                    self.table.powerEntered = True
 
-                if table.angleEntered and table.powerEntered:
-                    ball1.move(table.angle, table.power)
-                    table.angleEntered = False
-                    table.powerEntered = False
-            options = pymunk.pygame_util.DrawOptions(table.screen)
-            table.space.debug_draw(options)
+                if self.table.angleEntered and self.table.powerEntered:
+                    ball1.move(self.table.angle, self.table.power)
+                    self.table.angleEntered = False
+                    self.table.powerEntered = False
+
+            doFriction(ball1) #this needs to apply to every ball
+
+            #here is where you write the state of the game to a csv file? lol
+
+            self.ready = self.checkDone()
 
 
-            doFriction(ball1)
-            # for item in table.space.bodies:
-            # #     if(type(item) == type(ball1)):
-            #     doFriction(item)
+            options = pymunk.pygame_util.DrawOptions(self.table.screen)
+            self.table.space.debug_draw(options)
             pygame.display.update()
-            table.space.step(1.0/60.0)
-            table.clock.tick(60)
+            self.table.space.step(2.0/60.0)
+            self.table.clock.tick(60)
 
     def callback(self, arbiter, space, data):
         ball = arbiter.shapes[0]
         hole = arbiter.shapes[1]
-        print(ball.body.mass)
-        # print((list(ball.body.shapes))[0].mass)
-        # print((list(ball.body.shapes))[1].mass)
-        # print((list(ball.body.shapes))[2].mass)
-        # print((list(ball.body.shapes))[3].mass)
+        self.sunk(ball)
         return False;
+
+    def sunk(self, ball):
+        t = pymunk.Space()
+        striped = False
+        if len(ball.body.shapes) == 4:
+            striped=True
+        num = math.ceil((ball.body.mass-1)*100000000)
+        self.points = self.points + 1
+        self.sunkBalls.append(ball)
+        index = self.sunkBalls.index(ball)
+        b = pymunk.Body()
+        # ball.body.body_type = 1
+        if num == 16:
+            ball.body.position = 225, 225
+        else:
+            ball.body.position=1025, 23+(index*23)
+        ball.body.velocity = (0,0)
+        print("Points", self.points)
+    def checkDone(self):
+        done = True
+        for s in self.table.space.bodies:
+            if (not s.velocity().x == 0) or (not s.velocity().y == 0):
+                done = False
+        return done
+
+
+
 
 def doFriction(ball):
     velX = ball.body.velocity.x
