690d6620f7188cd95b5921922e1e09074f11529a
nza3552 <nza3552@rit.edu>
2019-04-10 18:40:41 -0400

    stupid amount of changes

--- venv/Ball.py
+++ venv/Ball.py
@@ -10,29 +10,54 @@ from pygame.locals import *
 from TextInput import TextInput
 import pymunk
 import pymunk.pygame_util
+import pymunk.shapes
 
 
+f = pygame.font.Font(None, 32)
 class Ball:
     def __init__(self, num, col, striped):
-        self.radius = 23
-        self.mass = 5
+        self.frictionCounter = 0
+        self.radius = 23/2
+        self.mass = 1.00000016
         self.num = num
         self.col = col
         self.striped = striped
+
+
         self.moment = pymunk.moment_for_circle(self.mass, 0, self.radius, (0,0))
+        self.moment = math.inf
         self.body = pymunk.Body(self.mass, self.moment)
-        self.shape = pymunk.Circle(self.body, self.radius, (0, 0))
-        self.shape.friction = 50
+        self.body.body_type = 0
+
+        self.shapeID = pymunk.Circle(self.body, num, (0,0))
+
+
+        self.shape3 = pymunk.Circle(self.body, 1, (0,0))
+        if self.striped:
+            # self.shape3 = pymunk.Poly()
+            self.shape = pymunk.Circle(self.body, self.radius, (0, 0))
+            self.shape2 = pymunk.Poly(self.body, ((-self.radius,3), (self.radius,3), (self.radius,-3), (-self.radius,-3)))
+            self.shape.color = (255, 255, 255, 255)
+            self.shape2.color = (col)
+        else:
+            self.shape.color = (col)
+            self.shape = pymunk.Circle(self.body, self.radius, (0, 0))
+        self.shape.elasticity = 1
+        self.shape.friction = 0.0
+        self.shape.filter = pymunk.ShapeFilter(categories=1)
+        self.shape3.filter = pymunk.ShapeFilter(categories=4, mask=4)
+        self.shape3.collision_type = 10
 
     def place(self, space, loc):
         self.body.position = loc
-        space.add(self.body, self.shape)
+        space.add(self.body, self.shape, self.shape2, self.shape3)
+
     def move(self, angle, power):
         angle = math.radians(float(angle))
         x = math.cos(angle)
         y = math.sin(angle)
-        xPower = x*float(power)
-        yPower = y*float(power)
+        xPower = x*float(power)*10
+        yPower = y*float(power)*10
 
         print("moving")
         self.body.apply_impulse_at_local_point((xPower,yPower), (0,0))
--- venv/Pool.py
+++ venv/Pool.py
@@ -12,40 +12,114 @@ import pymunk
 import pymunk.pygame_util
 from Table import Table
 from Ball import Ball
+class Pool:
 
-def main():
-    table = Table()
-    ball1 = Ball(1, (255, 0, 0), True)
-    ball1.place(table.space, (200,200))
+    BALL_RADIUS = 23
 
-    ball2 = Ball(2, (255, 255, 0), True)
-    ball2.place(table.space, (300, 220))
-    while(True):
-        table.display()
-        events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT:
-                exit()
-        if table.textinput.update(events):
-            if table.textStatus == "Angle":
-                table.angle = table.textinput.input_string
-                table.textinput.clear_text()
-                table.textStatus = "Power"
-                table.angleEntered = True
-            else:
-                table.textStatus = "Angle"
-                table.power = table.textinput.input_string
-                table.textinput.clear_text()
-                table.powerEntered = True
+    FOOT_SPOT = (600, 200)
+    EIGHT_BALL_POS = (FOOT_SPOT[0] + BALL_RADIUS*2, FOOT_SPOT[1])
+    TRIANGLE_COORDS = [FOOT_SPOT,
+                       (FOOT_SPOT[0] + BALL_RADIUS, FOOT_SPOT[1] + BALL_RADIUS/2),
+                       (FOOT_SPOT[0] + BALL_RADIUS, FOOT_SPOT[1] + -BALL_RADIUS/2),
+                       (FOOT_SPOT[0] + BALL_RADIUS*2, FOOT_SPOT[1] + BALL_RADIUS),
+                       (FOOT_SPOT[0] + BALL_RADIUS*2, FOOT_SPOT[1] + -BALL_RADIUS),
+                       (FOOT_SPOT[0] + BALL_RADIUS*3, FOOT_SPOT[1] + BALL_RADIUS/2),
+                       (FOOT_SPOT[0] + BALL_RADIUS*3, FOOT_SPOT[1] + -BALL_RADIUS/2),
+                       (FOOT_SPOT[0] + BALL_RADIUS*3, FOOT_SPOT[1] + BALL_RADIUS*1.5),
+                       (FOOT_SPOT[0] + BALL_RADIUS*3, FOOT_SPOT[1] + -BALL_RADIUS*1.5),
+                       (FOOT_SPOT[0] + BALL_RADIUS*4, FOOT_SPOT[1] + 0),
+                       (FOOT_SPOT[0] + BALL_RADIUS*4, FOOT_SPOT[1] + BALL_RADIUS),
+                       (FOOT_SPOT[0] + BALL_RADIUS*4, FOOT_SPOT[1] + BALL_RADIUS*2),
+                       (FOOT_SPOT[0] + BALL_RADIUS*4, FOOT_SPOT[1] + -BALL_RADIUS),
+                       (FOOT_SPOT[0] + BALL_RADIUS*4, FOOT_SPOT[1] + -BALL_RADIUS*2)]
+    BALL_INFO = [
+        (1, (255, 255, 0), False),
+        (2, (0, 0, 255), False),
+        (3, (255, 0, 0), False),
+        (4, (138, 43, 226), False),
+        (5, (255, 69, 0), False),
+        (6, (0, 128, 0), False),
+        (7, (128, 0, 0), False),
+        (9, (255, 255, 0), True),
+        (8, (0, 0, 0), False),
+        (10, (0, 0, 255), True),
+        (11, (255, 0, 0), True),
+        (12, (138, 43, 226), True),
+        (13, (255, 69, 0), True),
+        (14, (0, 128, 0), True),
+        (15, (128, 0, 0), True),
+        (16, (255, 255, 255), False)]
 
-            if table.angleEntered and table.powerEntered:
-                ball1.move(table.angle, table.power)
-                table.angleEntered = False
-                table.powerEntered = False
-        table.space.debug_draw(pymunk.pygame_util.DrawOptions(table.screen))
-        pygame.display.update()
-        table.space.step(1.0/60.0)
-        table.clock.tick(60)
+    def main(self):
+        table = Table()
+
+        ball1 = Ball(16, (255, 0, 0, 255), True)
+        ball1.place(table.space, (200,200))
+
+        # ball2 = Ball(2, (255, 255, 0), True)
+        # ball2.place(table.space, (300, 220))
+        collHand = table.space.add_collision_handler(10, 11)
+        collHand.begin = self.callback
+        while(True):
+            # print(collHand.separate)
+            # while(not collHand.separate == None):
+            #     print(collHand.separate)
+            table.display()
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    exit()
+            if table.textinput.update(events):
+                if table.textStatus == "Angle":
+                    table.angle = table.textinput.input_string
+                    table.textinput.clear_text()
+                    table.textStatus = "Power"
+                    table.angleEntered = True
+                else:
+                    table.textStatus = "Angle"
+                    table.power = table.textinput.input_string
+                    table.textinput.clear_text()
+                    table.powerEntered = True
+
+                if table.angleEntered and table.powerEntered:
+                    ball1.move(table.angle, table.power)
+                    table.angleEntered = False
+                    table.powerEntered = False
+            options = pymunk.pygame_util.DrawOptions(table.screen)
+            table.space.debug_draw(options)
 
 
-main()
+            doFriction(ball1)
+            # for item in table.space.bodies:
+            # #     if(type(item) == type(ball1)):
+            #     doFriction(item)
+            pygame.display.update()
+            table.space.step(1.0/60.0)
+            table.clock.tick(60)
+
+    def callback(self, arbiter, space, data):
+        ball = arbiter.shapes[0]
+        hole = arbiter.shapes[1]
+        print(ball.body.mass)
+        # print((list(ball.body.shapes))[0].mass)
+        # print((list(ball.body.shapes))[1].mass)
+        # print((list(ball.body.shapes))[2].mass)
+        # print((list(ball.body.shapes))[3].mass)
+        return False;
+
+def doFriction(ball):
+    velX = ball.body.velocity.x
+    velY = ball.body.velocity.y
+    # print(ball.body.velocity)
+    if abs(velX)<5 and abs(velY)<5:
+        ball.frictionCounter = ball.frictionCounter + 1
+        # print("BALL COUNTER: ", ball.frictionCounter)
+        if ball.frictionCounter > 30:
+            velX = 0
+            velY = 0
+    else:
+        ball.frictionCounter = 0
+    ball.body.velocity = pymunk.Vec2d(velX, velY)
+
+p = Pool()
+Pool.main(p)
